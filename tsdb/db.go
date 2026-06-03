515fed6043434f3c59eb164bd045a44a4f33b446
Deirdre Malone <dmalone@redhat.com>
2026-06-03 11:45:39 +0100

    [release-4.14] OCPBUGS-86878: Bump google.golang.org/grpc

--- tsdb/db.go
+++ tsdb/db.go
@@ -579,8 +579,14 @@ func (db *DBReadOnly) Blocks() ([]BlockReader, error) {
 		return nil, nil
 	}
 
-	slices.SortFunc(loadable, func(a, b *Block) bool {
-		return a.Meta().MinTime < b.Meta().MinTime
+	slices.SortFunc(loadable, func(a, b *Block) int {
+		if a.Meta().MinTime < b.Meta().MinTime {
+			return -1
+		}
+		if a.Meta().MinTime > b.Meta().MinTime {
+			return 1
+		}
+		return 0
 	})
 
 	blockMetas := make([]BlockMeta, 0, len(loadable))
@@ -1445,8 +1451,14 @@ func (db *DB) reloadBlocks() (err error) {
 	}
 	db.metrics.blocksBytes.Set(float64(blocksSize))
 
-	slices.SortFunc(toLoad, func(a, b *Block) bool {
-		return a.Meta().MinTime < b.Meta().MinTime
+	slices.SortFunc(toLoad, func(a, b *Block) int {
+		if a.Meta().MinTime < b.Meta().MinTime {
+			return -1
+		}
+		if a.Meta().MinTime > b.Meta().MinTime {
+			return 1
+		}
+		return 0
 	})
 
 	// Swap new blocks first for subsequently created readers to be seen.
@@ -1515,8 +1527,14 @@ func deletableBlocks(db *DB, blocks []*Block) map[ulid.ULID]struct{} {
 
 	// Sort the blocks by time - newest to oldest (largest to smallest timestamp).
 	// This ensures that the retentions will remove the oldest  blocks.
-	slices.SortFunc(blocks, func(a, b *Block) bool {
-		return a.Meta().MaxTime > b.Meta().MaxTime
+	slices.SortFunc(blocks, func(a, b *Block) int {
+		if a.Meta().MaxTime > b.Meta().MaxTime {
+			return -1
+		}
+		if a.Meta().MaxTime < b.Meta().MaxTime {
+			return 1
+		}
+		return 0
 	})
 
 	for _, block := range blocks {
